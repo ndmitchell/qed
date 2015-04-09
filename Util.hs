476c9c193412bba571d0909c879027cf8657dca5
Neil Mitchell <ndmitchell@gmail.com>
2015-04-09 14:54:49 +0100

    Delete utilities which were unused

--- Util.hs
+++ Util.hs
@@ -3,35 +3,20 @@
 -- | Generic utilities.
 module Util(module Util, module Safe, trace) where
 
-import System.Info
-import System.Process
-import Control.Exception
-import System.Exit
-import Data.Function
 import Data.List
 import Control.Monad.State
 import Data.IORef
 import Debug.Trace
 import System.IO.Unsafe
 import Data.Time.Clock.POSIX(getPOSIXTime)
-import System.Directory
-import System.FilePath
-import Data.Char
-import System.IO
-import GHC.IO.Handle(hDuplicate,hDuplicateTo)
 import Safe
 import Numeric
-import Control.Arrow
 import System.Environment
+import Data.Tuple.Extra
 
 
 rlookup :: Eq a => a -> [(b,a)] -> Maybe b
 rlookup x y = lookup x $ map swap y
-
-swap (x,y) = (y,x)
-
-sortOn :: Ord b => (a -> b) -> [a] -> [a]
-sortOn f = sortBy (compare `on` f)
 
 
 subset x y = null $ x \\ y
@@ -120,81 +105,6 @@ resetTime :: a -> a
 resetTime x = unsafePerformIO $ do
     writeIORef timeRef 0
     return x
-
-
-type Id x = x -> x
-
-getDirectoryContentsRecursive :: FilePath -> IO [FilePath]
-getDirectoryContentsRecursive dir = do
-    xs <- getDirectoryContents dir
-    (dirs,files) <- partitionM doesDirectoryExist [dir </> x | x <- xs, not $ isBadDir x]
-    rest <- concatMapM getDirectoryContentsRecursive $ sort dirs
-    return $ sort files ++ rest
-    where
-        isBadDir x = "." `isPrefixOf` x || "_" `isPrefixOf` x
-
-
-partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a], [a])
-partitionM f [] = return ([], [])
-partitionM f (x:xs) = do
-    res <- f x
-    (as,bs) <- partitionM f xs
-    return ([x | res]++as, [x | not res]++bs)
-
-concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
-concatMapM f = liftM concat . mapM f
-
-lower = map toLower
-upper = map toUpper
-
-captureOutput :: IO () -> IO String
-captureOutput act = do
-    tmp <- getTemporaryDirectory
-    (f,h) <- openTempFile tmp "hlint"
-    sto <- hDuplicate stdout
-    ste <- hDuplicate stderr
-    hDuplicateTo h stdout
-    hDuplicateTo h stderr
-    hClose h
-    act
-    hDuplicateTo sto stdout
-    hDuplicateTo ste stderr
-    res <- readFile' f
-    removeFile f
-    return res
-
-captureStdout :: IO () -> IO String
-captureStdout act = do
-    tmp <- getTemporaryDirectory
-    (f,h) <- openTempFile tmp "hlint"
-    sto <- hDuplicate stdout
-    hDuplicateTo h stdout
-    hClose h
-    act
-    hDuplicateTo sto stdout
-    res <- readFile' f
-    removeFile f
-    return res
-
-readFile' :: FilePath -> IO String
-readFile' x = listM' =<< readFile x
-
-listM' :: Monad m => [a] -> m [a]
-listM' x = length x `seq` return x
-
-devNull = if os == "mingw32" then "nul" else "/dev/null"
-
-withDirectory new act = do
-    old <- getCurrentDirectory
-    bracket_
-        (setCurrentDirectory new)
-        (setCurrentDirectory old)
-        act
-
-system_ cmd = do
-    putStrLn cmd
-    res <- system cmd
-    when (res /= ExitSuccess) $ error $ "system command failed: " ++ cmd
 
 fast = "--fast" `elem` unsafePerformIO getArgs
 
