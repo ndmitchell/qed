ab31c9ae5369924b2a32f45d42c91b764316221f
Neil Mitchell <ndmitchell@gmail.com>
2015-10-16 14:35:41 +0100

    Work on writing an interpreter

--- Main.hs
+++ Main.hs
@@ -2,9 +2,13 @@
 
 module Main(main) where
 
-import Language.Haskell.Exts
+import Language.Haskell.Exts hiding (parse)
 import Data.Generics.Uniplate.Data
 import Data.List.Extra
+import Data.Tuple.Extra
+import Control.Monad
+import Prop
+import Exp
 import Proofy
 
 -- TODO: Source the definitions and data types from haskell98
@@ -33,10 +37,10 @@ addDefine file = do
         add :: Decl -> IO ()
         add x@ClassDecl{} = mapM_ add $ children x
         add x@FunBind{} = do
-            putStrLn $ prettyPrint x
+            -- putStrLn $ prettyPrint x
             define $ prettyPrint x
         add x@PatBind{} = do
-            putStrLn $ prettyPrint x
+            -- putStrLn $ prettyPrint x
             define $ prettyPrint x
         add x@DataDecl{} = define $ prettyPrint x
         add InfixDecl{} = return ()
@@ -48,4 +52,50 @@ addDefine file = do
 
 
 addTheorem :: FilePath -> IO ()
-addTheorem = undefined
+addTheorem file = do
+    src <- parseTheorem <$> readFile file
+    forM_ src $ \x -> case x of
+        Law{} -> return ()
+        Prove{} -> return ()
+        Proof p xs -> void $ proofProp p $ mapM_ evalStep xs
+
+data Theorem
+    = Proof Prop [Step]
+    | Law String [Prop]
+    | Prove String String [(String, String)]
+      deriving Show
+
+data Step = Step String deriving Show
+
+evalStep :: Step -> IO ()
+evalStep = undefined
+
+parseTheorem :: String -> [Theorem]
+parseTheorem = map f . lexer
+    where
+        f (a,b) = case words a of
+            "forall":_ -> Proof (parseProp a) $ map parseStep b
+            ["law",a] -> Law a $ map parseProp b
+            ["prove",a,a2] -> Prove a a2 $ map parseEq b
+            _ -> error $ "Unknown keyword, " ++ a
+
+parseStep :: String -> Step
+parseStep = Step
+
+parseProp :: String -> Prop
+parseProp x = Prop (map V $ delete "forall" $ words quant) (parse prf1) (parse $ drop 3 prf2)
+    where
+        (quant, '.':prf) = breakOn "." x
+        (prf1, prf2) = breakOn " = " prf
+
+parseEq :: String -> (String, String)
+parseEq = both trim . second (drop 3) . breakOn " = "
+
+lexer :: String -> [(String, [String])]
+lexer = f . filter (/= "") . map (trimEnd . uncomment) . lines . replace "\t" "    "
+    where
+        uncomment x = maybe x fst $ stripInfix " -- " x
+
+        f [] = []
+        f (x:xs) = (x,map trimStart a) : f b
+            where (a,b) = break (not . isPrefixOf " ") xs
