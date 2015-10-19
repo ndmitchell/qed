c5476147594c0e7fe57d11ea40c95b47f6d087a4
Neil Mitchell <ndmitchell@gmail.com>
2015-10-19 18:09:05 +0100

    Delete unused pieces from Util

--- src/Proof/Util.hs
+++ src/Proof/Util.hs
@@ -1,38 +1,17 @@
 {-# LANGUAGE PatternGuards #-}
 
 -- | Generic utilities.
-module Proof.Util(module Proof.Util, headNote, trace) where
+module Proof.Util(module Proof.Util, headNote) where
 
-import Data.List
-import Debug.Trace
 import System.IO.Unsafe
 import Safe
 import System.Environment
 import Control.DeepSeq
-import Data.Tuple.Extra
+
 
 rnf2 a b = rnf a `seq` rnf b
 rnf3 a b c = rnf a `seq` rnf b `seq` rnf c
 rnf4 a b c d = rnf a `seq` rnf b `seq` rnf c `seq` rnf d
-
-rlookup :: Eq a => a -> [(b,a)] -> Maybe b
-rlookup x y = lookup x $ map swap y
-
-Just a =^= Just b = a == b
-_ =^= _ = False
-
-
-subset x y = null $ x \\ y
-
-fixEq f x = if x == x2 then x else fixEq f x2
-    where x2 = f x
-
-
-delFst :: Eq a => a -> [(a,b)] -> [(a,b)]
-delFst x = filter ((/=) x . fst)
-
-delFsts :: Eq a => [a] -> [(a,b)] -> [(a,b)]
-delFsts x = filter (flip notElem x . fst)
 
 
 fast = "--fast" `elem` unsafePerformIO getArgs
