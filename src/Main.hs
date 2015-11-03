431d7149f09fafb19a2c3b7a1d156f8356535303
Neil Mitchell <ndmitchell@gmail.com>
2015-11-03 20:08:21 +0000

    Move the useful todo functions into Direct

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,14 @@ import Classes
 import HLint
 import Proof.QED
 
+-- TODO: Formalise generalise properly. Can it be done as extraction? Is strict generalisation always sufficient?
+-- TODO: Write an automatic prover
+
+-- TOTHINK: What to do about type classes? How do you prove laws about instances? How do you associate laws?
+--          Add an assume to a proof?
+-- TOTHINK: What do I do about things like +? Do I assume them like I do for type classes?
+-- TOTHINK: Do I just state the laws and leave it at that?
+
 main = qedCheat $ do
     imports "Builtin"
     imports "Prelude"
